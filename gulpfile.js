5bf6931bbddac469c58e755dc0062dace5aa7f29
damilola_ogungbesan <damilola_ogungbesan@venturegardengroup.com>
2018-02-24 12:26:47 +0100

    First Basic Gulp Setup

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat')
+
+
+gulp.task('makeCopy', function(){
+    return gulp.src('src/*.html')
+           .pipe(gulp.dest('dist'));
+});
+
+gulp.task('imagemin', function(){
+    return gulp.src('src/assets/img/*')
+            .pipe(imagemin())
+            .pipe(gulp.dest('dist/assets/img/'))
+});
+
+gulp.task('jsminify', function(){
+    gulp.src('src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/assets/js'))
+});
+
+gulp.task('script', function(){
+    gulp.src('src/assets/js/*.js')
+        .pipe(concat('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/assests/js'))
+})
+
+gulp.task('sass', function(){
+    return gulp.src('src/assets/scss/*.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest('dist/css/'))
+});
+
+
+
+gulp.task('default', ['makeCopy', 'imagemin', 'sass', 'script'])
+
+
+gulp.task('watch',function(){
+    gulp.watch('src/assets/js/*.js', ['script']);
+    gulp.watch('src/assets/sass/*.js', ['sass']);
+    gulp.watch('src/assets/img/*', ['imagemin']);
+    gulp.watch('src/*.html', ['makeCopy']);
+})
